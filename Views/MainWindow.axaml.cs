8a89087a396da55ac186d58193484bcdfe955fd7
raddevus <1radeutsch1@gmail.com>
2025-12-21 12:37:44 -0500

    Got basics of button click working

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace BasicAvalonia.Views;
 
@@ -7,5 +8,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        System.Console.WriteLine($"Click! Celsius={Celsius.Text}");
     }
 }
